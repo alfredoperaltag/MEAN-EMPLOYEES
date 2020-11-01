d21beb75342df2be14ee74c656cc2f90a0c0a8c9
alfredoperaltag <alfredo08peralta@gmail.com>
2020-11-01 00:12:02 -0600

    frontend desing

--- frontend/src/app/components/employees/employees.component.ts
+++ frontend/src/app/components/employees/employees.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { Employee } from 'src/app/models/employee';
 
 import { EmployeeService } from '../../services/employee.service'
+import { NgForm } from '@angular/forms';
+import { Employee } from '../../models/employee'
 
-declare var M: any
+//declare var M: any
 
 @Component({
   selector: 'app-employees',
@@ -13,49 +13,42 @@ declare var M: any
   providers: [EmployeeService]
 })
 export class EmployeesComponent implements OnInit {
-
   constructor(public employeeService: EmployeeService) { }
 
   ngOnInit(): void {
     this.getEmployees()
   }
 
-  addEmployee(form: NgForm) {
+  addEmployee(form?: NgForm) {
     if (form.value._id) {
-      this.employeeService.putEmployee(form.value)
-        .subscribe(res => {
-          this.resetForm(form)
-          M.toast({ html: 'Updated Successfully' })
-          this.getEmployees()
-        })
+      this.employeeService.putEmployee(form.value).subscribe(res => {
+        this.resetForm(form)
+        this.getEmployees()
+      })
     } else {
-      this.employeeService.postEmployee(form.value)
-        .subscribe(res => {
-          this.resetForm(form)
-          M.toast({ html: 'Save Successfully' })
-          this.getEmployees()
-        })
+      this.employeeService.postEmployee(form.value).subscribe(res => {
+        this.getEmployees()
+        this.resetForm(form)
+      })
     }
   }
 
   getEmployees() {
-    this.employeeService.getEmployees()
-      .subscribe(res => {
-        this.employeeService.employees = res as Employee[]
-        console.log(res);
-      })
+    this.employeeService.getEmployees().subscribe(res => {
+      this.employeeService.employees = res
+    })
   }
 
   editEmployee(employee: Employee) {
     this.employeeService.selectedEmployee = employee
   }
 
-  deleteEmployee(_id: string) {
+  deleteEmployee(_id: string, form: NgForm) {
     if (confirm('Are you sure you want to delete it?')) {
       this.employeeService.deleteEmployee(_id)
         .subscribe(res => {
           this.getEmployees()
-          M.toast({ html: 'Deleted Successfully' })
+          this.resetForm(form)
         })
     }
   }
--- frontend/src/app/services/employee.service.ts
+++ frontend/src/app/services/employee.service.ts
@@ -1,33 +1,33 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
-import { Employee } from '../models/employee';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+import { Employee } from "../models/employee";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class EmployeeService {
-
-  selectedEmployee: Employee
-  employees: Employee[]
-  readonly URL_API = 'http://localhost:3000/api/employees'
+  selectedEmployee: Employee;
+  employees: Employee[];
+  readonly URL_API = "http://localhost:3000/api/employees";
 
   constructor(private http: HttpClient) {
-    this.selectedEmployee = new Employee()
+    this.selectedEmployee = new Employee();
+  }
+
+  postEmployee(employee: Employee) {
+    return this.http.post(this.URL_API, employee);
   }
 
   getEmployees() {
-    return this.http.get(this.URL_API)
-  }
-
-  postEmployee(Employee: Employee) {
-    return this.http.post(this.URL_API, Employee)
+    return this.http.get<Employee[]>(this.URL_API);
   }
 
   putEmployee(employee: Employee) {
-    return this.http.put(this.URL_API + `/${employee._id}`, employee)
+    return this.http.put(this.URL_API + `/${employee._id}`, employee);
   }
 
   deleteEmployee(_id: string) {
-    return this.http.delete(this.URL_API + `/${_id}`)
+    return this.http.delete(this.URL_API + `/${_id}`);
   }
 }
